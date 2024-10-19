7908df8926e60d9832b18d7b37ad7d0b62ca695d
alanmulero <alan.mulero@gmail.com>
2024-10-19 13:17:57 -0300

    Variaveis e objeto

---
+++ src/Componentes/Variaveis.js
@@ -0,0 +1,20 @@
+
+const Variaveis = () =>{
+    const nome = "Jujuba";
+    const pessoa = {
+        atividade: "estudante",
+        idade: 16,
+    };
+
+    return (
+
+        <div>
+            <h1>Nome: {nome}</h1>
+            <h2>Objeto: {pessoa.idade}</h2>
+            <p>Ocupação: {pessoa.atividade}</p>
+        </div>
+
+    )
+}
+
+export default Variaveis;

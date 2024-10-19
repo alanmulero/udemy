3504bfad6d012cf95f268dc8bbf13a565ce76738
alanmulero <alan.mulero@gmail.com>
2024-10-19 17:19:49 -0300

    Testes com componentes e reaproveitamento de componentes.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Variaveis from './Componentes/Variaveis';
-
+import Reaproveitamento from './Componentes/Reaproveitamento/Reaproveitamento'
 function App() {
   return (
     <div className="App">
       <p>Testes.</p>
       <Variaveis />
+      <Reaproveitamento />
    
     </div>
   );

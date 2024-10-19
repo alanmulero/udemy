a297634a623cea16c5406bd0f51699c169a78d37
alanmulero <alan.mulero@gmail.com>
2024-10-19 17:53:19 -0300

    teste pastas

---
+++ src/Componentes/Reaproveitamento/Reaproveitamento.js
@@ -0,0 +1,15 @@
+const Reaproveitamento = () => {
+
+	return(
+		<div>
+		<h3> Reaproveitando componente </h3>
+
+		<p>teste 2</p>
+		<h4>teste 4</h4>
+		</div>
+		)
+
+
+}
+
+export default Reaproveitamento;
